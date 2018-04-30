6fc6e5412f063c631f6727b293cf50d6f4aaba7f
Michael Irvine <michael.f.irvine85@gmail.com>
2018-04-30 12:21:51 -0700

    adding waypoints

--- js/kanata-script.js
+++ js/kanata-script.js
@@ -2,4 +2,28 @@ jQuery(document).ready(function($){
 
 console.log('yoooo this is the Kanata Cafe Site!');
 
+// Background change-to-pink to pink
+
+const waypoint = new Waypoint({
+  element: document.getElementById(".site-content"),
+  handler: function(direction) {
+    alert('work damnit');
+  }
+});
+
+
+
+$waypoint.waypoint(
+  function(direction) {
+    if (direction == "down") {
+      $waypoint.addClass("change-to-pink");
+    } else {
+      $waypoint.removeClass("change-to-pink");
+    }
+  },
+  { offset: "35%" }
+);
+
+console.log('does this work?');
+
 });
