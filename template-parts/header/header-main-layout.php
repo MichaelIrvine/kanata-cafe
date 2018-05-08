1cae4161bb432ea19bf8be3e4ddd9af9c1bf1f5a
Michael Irvine <michael.f.irvine85@gmail.com>
2018-05-08 10:07:29 -0700

    adding styling to menu and mobile button

--- template-parts/header/header-main-layout.php
+++ template-parts/header/header-main-layout.php
@@ -30,17 +30,35 @@
 
 				if (function_exists('get_field')){
 
-					$imageLeft  = get_field('cover_left');
-					$imageRight = get_field('cover_right');
-					$size       = 'front-cover';
+					$imageLeft   = get_field('cover_left');
+					$imageRight  = get_field('cover_right');
+					$size        = 'front-cover';
 				?>
 			<div class="hero-wrap">
 			<div class="hero-left" style="background-image: url(<?php echo $imageLeft; ?> )"></div>
 			<div class="hero-right" style="background-image: url(<?php echo $imageRight; ?> )"></div>
 			</div>
 			<?php
-			
-				}
+				echo '<div class="big-text-container">';
+				// check if the repeater field has rows of data
+					if( have_rows('welcome_text') ):
+				// loop through the rows of data
+    					while ( have_rows('welcome_text') ) : the_row();
+				// display a sub field value
+        				echo '<h1 class="fp_english_big">';the_sub_field('fp_english_big');echo '</h1>';
+						echo '<h1 class="fp_japanese_big">';the_sub_field('fp_japanese_big');echo '</h1>';
+				echo '</div>';
+    endwhile;
+
+else :
+
+    // no rows found
+
+endif;
+				} //This closes the above IF statement
 			?>
+
+
+
 	</div> <!-- Main Header Bar -->
 </div> <!-- Main Header Bar Wrap -->
